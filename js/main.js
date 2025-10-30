c65168ecb62a64a4be3774edeee5742e38133801
kam <148465412+raspy-windbird@users.noreply.github.com>
2025-10-31 04:00:53 +0900

    update

--- js/main.js
+++ js/main.js
@@ -19,7 +19,7 @@ function boot () {
     const button_id = "boot_button";
     Dom.create("input", "boot", { type: "button", value: "SYSTEM", id: button_id });
     const button = document.getElementById(button_id);
-    Dom.clicked(button,butoon.remove(),"true");
+    Dom.clicked(button,button.remove(),"true");
 }
 
 main();
