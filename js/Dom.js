fc8b10bcd8912b173d76da907e2ee3a9356af8af
kam <148465412+raspy-windbird@users.noreply.github.com>
2025-10-31 02:50:26 +0900

    update

--- js/Dom.js
+++ js/Dom.js
@@ -1,9 +1,11 @@
 class dom {
+/**
+ * 
+ * @param {Text} ele 作成要素のhtmlタグ
+ * @param {Text} origin_id 親とする要素のid
+ * @param {JSON} attribute 変更する要素を配列で
+ */
 async create (ele,origin_id,attribute) {
-    // ele: 作りたい要素のタグ「h1」など
-    // origin_id: 親となる要素のid
-    // attribute: 変更したい要素を「value,hellow」のようなペアで入れる
-    
     const element = document.createElement(ele);
     
     for (let key in attribute){
@@ -12,7 +14,12 @@ async create (ele,origin_id,attribute) {
 
     document.getElementById(origin_id).appendChild(element);
 }
-
+/**
+ * 
+ * @param {Text} dom 検知する要素
+ * @param {Function} dothing クリックされた場合の動作
+ * @param {Boolean} One 一度のみの実行か
+ */
 async clicked (dom,dothing,One){
     document.addEventListener("DOMContentLoaded",() => {
     if (dom) {
