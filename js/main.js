53a628b7325882740c7147a65854e77fea9373be
kam <148465412+raspy-windbird@users.noreply.github.com>
2025-10-31 02:37:12 +0900

    update

--- js/main.js
+++ js/main.js
@@ -4,21 +4,20 @@ const Dom = new dom;
 async function main () {
     console.log("main ---LOADING startup---")
     await startup();
-
     await boot();
 }
 
 async function startup () {
     console.log("startup ---STANDBY---");
-    function dosomething(){console.log("clicked")}
-    Dom.clicked(document,dosomething,"true");
+    function dosomething(){console.log("hello!")}
+    await Dom.clicked(document,dosomething,"true")
 }
 
 async function boot (params) {
     const button_id = "boot";
     Dom.create("input","boot",{type:"button",value:"SYSTEM",id:button_id});
     const button = document.getElementById(button_id);
-    function loglog() {console.log("click")}
+    function loglog() {dom.remove();}
     Dom.clicked(button,loglog,"true");
 
 }
