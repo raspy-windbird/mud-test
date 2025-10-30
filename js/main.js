8443c542a34dffa2b997dd26bc598d5459f9ca63
kam <148465412+raspy-windbird@users.noreply.github.com>
2025-10-31 02:58:00 +0900

    update

--- js/main.js
+++ js/main.js
@@ -4,13 +4,15 @@ const Dom = new dom;
 async function main () {
     console.log("main ---LOADING startup---")
     await startup();
-    await boot();
 }
 
 async function startup () {
     console.log("startup ---STANDBY---");
-    function dosomething(){console.log("hello!")}
-    await Dom.clicked(document,dosomething,"true")
+    function clickdo(){
+        console.log("hello!")
+        boot()
+    }
+    Dom.clicked(document,clickdo,"true")
 }
 
 async function boot (params) {
