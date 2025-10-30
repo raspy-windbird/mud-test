114b39fadd8dfcfad4892f864519c563fb0ea972
kam <148465412+raspy-windbird@users.noreply.github.com>
2025-10-31 01:31:11 +0900

    Initialize main.js to load startup and boot modules

---
+++ js/main.js
@@ -0,0 +1,8 @@
+import {startup} from "./startup.js";
+import {boot} from "./boot.js";
+
+console.log("main.js ---LOADING startup.js---");
+startup();
+
+console.log("main.js ---LOADING boot.js---");
+boot();
